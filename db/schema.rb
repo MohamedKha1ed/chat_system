3181929ad332b54f5ba06d1613f1931090be564e
Mohamed Khaled <mohamed.khaled@pixelogicmedia.com>
2019-09-20 15:26:06 +0200

    add chats and messages

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190920120802) do
+ActiveRecord::Schema.define(version: 20190920132030) do
 
   create_table "applications", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
     t.string   "name"
@@ -19,4 +19,24 @@ ActiveRecord::Schema.define(version: 20190920120802) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "chats", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
+    t.integer  "number"
+    t.string   "name"
+    t.integer  "application_id"
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
+    t.index ["application_id"], name: "index_chats_on_application_id", using: :btree
+  end
+
+  create_table "messages", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
+    t.integer  "number"
+    t.text     "content",    limit: 65535
+    t.integer  "chat_id"
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
+    t.index ["chat_id"], name: "index_messages_on_chat_id", using: :btree
+  end
+
+  add_foreign_key "chats", "applications"
+  add_foreign_key "messages", "chats"
 end
